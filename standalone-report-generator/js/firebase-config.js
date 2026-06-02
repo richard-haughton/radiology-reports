18fbfdd4f69576ecd553fbbcc122367dcc7d56e6
dillonhaughton <dillon.richard.haughton@gmail.com>
2026-06-02 12:44:30 -0700

    Fix Firebase config to use compat SDK syntax

--- standalone-report-generator/js/firebase-config.js
+++ standalone-report-generator/js/firebase-config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBEDnwbmXDxktlCoYR88ShxRlp2abzw490",
   authDomain: "reports-eadf8.firebaseapp.com",
@@ -13,5 +7,7 @@ const firebaseConfig = {
   appId: "1:78116950355:web:8d96492b62ce812b5c51a6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+var appAuth = firebase.auth();
+var appDb = firebase.firestore();
